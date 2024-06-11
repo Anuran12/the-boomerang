aa58a6edcb00ca4f28625ef240efda2f6d73cf44
Anuran12 <anuranarts@gmail.com>
2024-06-11 17:49:21 +0530

    fix

--- server/index.js
+++ server/index.js
@@ -1,37 +1,35 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
-const cors = require('cors');
-require('dotenv/config');
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+// const cors = require('cors');
+require("dotenv/config");
 
-
-app.use(cors());
-app.options('*', cors())
+// app.use(cors());
+// app.options('*', cors())
 
 //middleware
 app.use(bodyParser.json());
 app.use(express.json());
 
-
 //Routes
-const userRoutes = require('./routes/user.js');
-const categoryRoutes = require('./routes/categories');
-const subCatRoutes = require('./routes/subCat.js');
-const productRoutes = require('./routes/products');
-const imageUploadRoutes = require('./helper/imageUpload.js');
-const productWeightRoutes = require('./routes/productWeight.js');
-const productRAMSRoutes = require('./routes/productRAMS.js');
-const productSIZESRoutes = require('./routes/productSIZE.js');
-const productReviews = require('./routes/productReviews.js');
-const cartSchema = require('./routes/cart.js');
-const myListSchema = require('./routes/myList.js');
-const ordersSchema = require('./routes/orders.js');
-const homeBannerSchema = require('./routes/homeBanner.js');
-const searchRoutes = require('./routes/search.js');
+const userRoutes = require("./routes/user.js");
+const categoryRoutes = require("./routes/categories");
+const subCatRoutes = require("./routes/subCat.js");
+const productRoutes = require("./routes/products");
+const imageUploadRoutes = require("./helper/imageUpload.js");
+const productWeightRoutes = require("./routes/productWeight.js");
+const productRAMSRoutes = require("./routes/productRAMS.js");
+const productSIZESRoutes = require("./routes/productSIZE.js");
+const productReviews = require("./routes/productReviews.js");
+const cartSchema = require("./routes/cart.js");
+const myListSchema = require("./routes/myList.js");
+const ordersSchema = require("./routes/orders.js");
+const homeBannerSchema = require("./routes/homeBanner.js");
+const searchRoutes = require("./routes/search.js");
 
-app.use("/api/user",userRoutes);
-app.use("/uploads",express.static("uploads"));
+app.use("/api/user", userRoutes);
+app.use("/uploads", express.static("uploads"));
 app.use(`/api/category`, categoryRoutes);
 app.use(`/api/subCat`, subCatRoutes);
 app.use(`/api/products`, productRoutes);
@@ -46,19 +44,19 @@ app.use(`/api/orders`, ordersSchema);
 app.use(`/api/homeBanner`, homeBannerSchema);
 app.use(`/api/search`, searchRoutes);
 
-
 //Database
-mongoose.connect(process.env.CONNECTION_STRING, {
+mongoose
+  .connect(process.env.CONNECTION_STRING, {
     useNewUrlParser: true,
-    useUnifiedTopology: true
-})
-    .then(() => {
-        console.log('Database Connection is ready...');
-        //Server
-        app.listen(process.env.PORT, () => {
-            console.log(`server is running http://localhost:${process.env.PORT}`);
-        })
-    })
-    .catch((err) => {
-        console.log(err);
-    })
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Database Connection is ready...");
+    //Server
+    app.listen(process.env.PORT, () => {
+      console.log(`server is running http://localhost:${process.env.PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
