c340f0e21abdfb138ff2b3a941a57c1b1d5324a6
Anuran12 <anuranarts@gmail.com>
2024-06-26 16:49:38 +0530

    logo

--- client/src/Components/Header/index.js
+++ client/src/Components/Header/index.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useRef, useState } from "react";
 import { Link } from "react-router-dom";
-import Logo from "../../assets/images/logo.jpg";
+import Logo from "../../assets/images/BOOMERANG.png";
 import Button from "@mui/material/Button";
 import CountryDropdown from "../CountryDropdown";
 import { FiUser } from "react-icons/fi";
